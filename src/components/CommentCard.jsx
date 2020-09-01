a942b67ec42475a58e74bdc0c208cb736db8b19b
austinbooth <austinbooth78@gmail.com>
2020-09-01 16:59:39 +0100

    Completed logic to up and down vote article comments

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import { render } from "@testing-library/react";
+import * as api from "../api";
 
-const CommentCard = (props) => {
-  const { comment_id, author, votes, created_at, body } = props.comment;
-  return <li className="article-comment">{body}</li>;
-};
+class CommentCard extends Component {
+  state = { ...this.props.comment, optimisticVotes: 0 };
+
+  render() {
+    const { comment_id, author, votes, created_at, body } = this.state;
+    const date = api.formatDate(created_at);
+
+    const modifyVotes = (votes) => {
+      api.patchVotes("comment", comment_id, votes);
+      this.setState((currentState) => {
+        currentState.votes += votes;
+        return {
+          ...currentState,
+          optimisticVotes: currentState.optimisticVotes + votes,
+        };
+      });
+    };
+
+    return (
+      <li className="article-comment">
+        {body}
+        <section className="comment-meta-data">
+          <h4>By {author}</h4>
+          <h4>At {date}</h4>
+          <h4>{votes} votes</h4>
+          <section className="up-down-vote-buttons">
+            <button
+              disabled={this.state.optimisticVotes === 1}
+              onClick={() => modifyVotes(1)}
+            >
+              Up vote
+            </button>
+            <button
+              disabled={this.state.optimisticVotes === -1}
+              onClick={() => modifyVotes(-1)}
+            >
+              Down vote
+            </button>
+          </section>
+        </section>
+      </li>
+    );
+  }
+}
 
 export default CommentCard;
